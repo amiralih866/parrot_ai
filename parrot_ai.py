ae943561ebe3b7a7fde9634efe3a9c78dfaf38e9
herom <sahk5486@gmail.com>
2025-04-25 19:47:22 +0330

    Add description to /start of bot

--- parrot_ai.py
+++ parrot_ai.py
@@ -8,7 +8,18 @@ TELEGRAM_BOT_TOKEN = ("7850863295:AAHZa5ZmSRAWa8br-IoXuFQ23S3P6BcW_9w")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! I am parrot bot 🤖 \n write: chat 'YOUR_MESSAGE'")
+    await update.message.reply_text("""
+    سلام به همه من ربات طوطی هستم که از هوش مصنوعی قدرت گرفته ام🦜🫡
+    فعلا به علت مشغله زیاد قابلیت های زیادی ندارم
+    مزیت من نسبت به ربات های دیگر اضافه شدن به گروه و پاسخ به اعضا است
+    .
+    شما میتوانید به راحتی من را به گروه خود اضافه کنید و با من مکالمه کنید
+    برای مکالمه کافیه بنویسید:
+    چت + "متن پیام مورد نظر خودتون"
+    تا من پاسخ شما رو همونجا بدم
+
+    اگر هم قابلیت های من رو فراموشی کردی میتونی دستور /start رو بزنی
+    """)
 
 
 app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
@@ -18,18 +29,12 @@ async def chat_with_parrot(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message.text.lower()
     if message.startswith('chat '):
         await update.message.reply_text('Thinking...')
-        if message.find('clock'):
-            await update.message.reply_text(str(datetime.datetime.now()).split(' ')[1].split('.')[0])
-        else:
-            reply = str(update.message).lower().split('chat ')[1]
-            await update.message.reply_text(str(parrot(reply)))
+        reply = str(update.message).lower().split('chat ')[1]
+        await update.message.reply_text(str(parrot(reply)))
     elif message.startswith('چت '):
         await update.message.reply_text('Thinking...')
-        if message.find('ساعت'):
-            await update.message.reply_text(str(datetime.datetime.now()).split(' ')[1].split('.')[0])
-        else:
-            reply = str(update.message).lower().split('چت ')[1]
-            await update.message.reply_text(str(parrot(reply)))
+        reply = str(update.message).lower().split('چت ')[1]
+        await update.message.reply_text(str(parrot(reply)))
 
 
 app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), chat_with_parrot))
